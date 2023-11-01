521237758def4fff8a457aeff177504411b591f5
gsrrgm <59813629+gsrrgm@users.noreply.github.com>
2023-11-02 01:32:40 +0200

    update task-7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import SimpleLightbox from 'simplelightbox';
+// import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+createMarckup(galleryItems);
+function createMarckup(array) {
+  const markup = array
+    .map(
+      ({ description, original, preview }) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+    </a>
+  </li>`
+    )
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', overlayOpacity: '0.9', showCounter: 'true', animationSpeed: '450', enableKeyboard: 'true', preloading: 'true',
+  });
